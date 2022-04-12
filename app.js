0499b3686e2d7479d56fcbb140cdc9cedbe89db6
Anand Mishra <anand.m@agiratech.com>
2022-04-12 18:49:27 +0530

    completed with responsive UI

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const dropdown = document.getElementById('navbarDropdownMenuLink');
+const sideNavDropdown = document.getElementById('sideNavDropdownMenuLink');
 
 const checklanguageHangler = () => {
     const langauge = localStorage.getItem('langauge');
-    if (langauge) dropdown.innerHTML = langauge;
+    if (langauge) {
+        dropdown.innerHTML = langauge;
+        sideNavDropdown.innerHTML = langauge;
+    }
     else {
         dropdown.innerHTML = 'English';
+        sideNavDropdown.innerHTML = 'English';
         localStorage.setItem('langauge', 'English');
     }
 }
 checklanguageHangler();
 const navdropdown = (value) => {
-    dropdown.innerHTML = value;
-    localStorage.setItem('langauge', value);
+    if (value) {
+        dropdown.innerHTML = value;
+        sideNavDropdown.innerHTML = value;
+        localStorage.setItem('langauge', value);
+    }
+}
+
+const openNav = () => {
+    document.getElementById("mySidenav").style.width = "45%";
+}
+
+const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
 }
